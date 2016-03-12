622518e6d79379d738fbe02f261b963cfe6c8d53
刘蕊 <1348045707@qq.com>
2016-03-12 14:59:21 -0800

    ckkkkkk
    LRRRRRRRR

---
+++ TestGitHubDemo/TestGitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitHubDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 LR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGitHubDemo/TestGitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitHubDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 LR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
